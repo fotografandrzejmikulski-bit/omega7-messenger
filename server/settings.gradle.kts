00d1e0307dfbca918e2f6ab073ef91ce39a2df9f
fotografandrzejmikulski-bit <fotografandrzejmikulski@gmail.com>
2026-09-03 18:21:40 +0200

    feat: add Ω7 relay Gradle module

---
+++ server/settings.gradle.kts
@@ -0,0 +1,3 @@
+pluginManagement { repositories { mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { mavenCentral() } }
+rootProject.name = "omega7-server"
